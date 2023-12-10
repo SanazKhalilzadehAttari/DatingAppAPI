c588ae4cf9bf1226e3145f1385a4407e6d70e8c0
Sanaz KhalilzadehAttari <khalilzadeh@integrationworks.de>
2023-12-10 16:48:27 +0100

    Add controller and Entities

---
+++ DatingAppAPI/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DatingAppAPI.Data;
+using DatingAppAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
